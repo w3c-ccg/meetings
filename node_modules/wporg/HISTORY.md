18abb731b55d47cbff999e1bce67fa1f819753cc
kimdhamilton <kimdhamilton@users.noreply.github.com>
2020-01-29 21:54:11 +0000

    Add html minutes for telecon [ci skip]

---
+++ node_modules/wporg/HISTORY.md
@@ -0,0 +1,4 @@
+History
+=======
+
+* 0.1.0 First Release - Covering Entire WordPress XML-RPC API
---
+++ node_modules/wporg/make.bat
@@ -0,0 +1,6 @@
+@ECHO ON
+node test/run.js
+@ECHO OFF
+ECHO **********************
+ECHO If the tests should fail, make sure you have SET the correct values for the enviroment variables in the file 'make.bat'
+ECHO **********************
