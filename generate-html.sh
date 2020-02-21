8a90983eee01de0f5819bcedc3297d036765de45
Kim (Hamilton) Duffy <kimdhamilton@gmail.com>
2020-02-21 16:10:42 -0500

    Update generate-html.sh

--- generate-html.sh
+++ generate-html.sh
@@ -4,8 +4,10 @@ if [[ -f "$DATE/irc.log" && ! -f "$DATE/index.html" ]]; then
     DIRECTORY=$DATE
 
     echo "Generating minutes for $DATE"
+
+    export GROUP=`cat $DIRECTORY/group.txt`
     # Generate minutes
-    node scribe-tool/index.js -d $DIRECTORY -m -i
+    node scribe-tool/index.js -d $DIRECTORY -g $GROUP -m -i
     git add index.html $DIRECTORY/index.html
 
 else
