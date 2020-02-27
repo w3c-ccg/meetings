07883ab75f21dcb0e21a219645416a91b85f584e
Kim (Hamilton) Duffy <kimdhamilton@gmail.com>
2020-02-27 12:42:14 -0800

    Update download-minutes.sh

--- download-minutes.sh
+++ download-minutes.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ ! -f "$DATE/irc-raw.log" ]; then
-    mkdir -p $DATE
+    mkdir -p $OUTPUT_DIR
     echo "....Downloading IRC logs for $DATE..."
-    curl -# "https://w3c-ccg.s3.digitalbazaar.com/minutes/$DATE-irc.log" > $DATE/irc-raw.log
-    ls -la $DATE
-    git add $DATE/irc-raw.log
-    echo $GROUP > $DATE/group.txt
-    git add $DATE/group.txt
+    curl -# "https://w3c-ccg.s3.digitalbazaar.com/minutes/$DATE-irc.log" > $OUTPUT_DIR/irc-raw.log
+    ls -la $OUTPUT_DIR
+    git add $OUTPUT_DIR/irc-raw.log
+    echo $GROUP > $OUTPUT_DIR/group.txt
+    git add $OUTPUT_DIR/group.txt
 else
   echo "nothing to do"
   exit 1
