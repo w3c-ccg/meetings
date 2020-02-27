c25cda5b2b1c03e30018e747630654254a657cbb
Kim (Hamilton) Duffy <kimdhamilton@gmail.com>
2020-02-27 12:50:31 -0800

    Update download-minutes.sh

--- download-minutes.sh
+++ download-minutes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -f "$DATE/irc-raw.log" ]; then
+if [ ! -f "$OUTPUT_DIR/irc-raw.log" ]; then
     mkdir -p $OUTPUT_DIR
     echo "....Downloading IRC logs for $DATE..."
     curl -# "https://w3c-ccg.s3.digitalbazaar.com/minutes/$DATE-irc.log" > $OUTPUT_DIR/irc-raw.log
